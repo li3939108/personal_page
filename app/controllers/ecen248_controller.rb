be1f7b0fed690acd24f0ec2fed0e64cbf8d8924c
me <li3939108@gmail.com>
2014-09-01 17:40:25 -0500

    ecen248

---
+++ app/controllers/ecen248_controller.rb
@@ -0,0 +1,6 @@
+class Ecen248Controller < ApplicationController
+	def show
+		# your code goes here
+		redirect_to 'http://people.tamu.edu/~/li3939108/ecen248', :status => :moved_permanently 
+	end
+end
