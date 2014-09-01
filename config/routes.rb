d1e9957f95f5af04f6cb0db50bd72dbf85d58951
me <li3939108@gmail.com>
2014-09-01 17:57:06 -0500

    ecen248

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ PersonalPage::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'ecen248/show' => 'ecen248#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
